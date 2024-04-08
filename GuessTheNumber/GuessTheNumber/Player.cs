aff7572a5cd806d7bd9d28dc1a2760ce05f26f2a
kittyxcode <katherine.pino059@gmail.com>
2024-04-08 14:03:56 -0400

    feat: create logic AIPlayer.cs

--- GuessTheNumber/GuessTheNumber/Player.cs
+++ GuessTheNumber/GuessTheNumber/Player.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 namespace GuessTheNumber
 {
-	public class Player
-	{
-		public Player()
-		{
-		}
-	}
+    public abstract class Player
+    /*
+     Características de la clase Player:
+    Propiedad que almacena el nombre.
+    Propiedad que almacena el último intento realizado por la jugadora.
+    Constructor que inicializa el último intento en cero y el nombre según el valor con el que fue instanciado.
+    Método que se encarga de hacer la predicción(MakeGuess()) solicitando por terminal un número y validando que sea correcto.*/
+
+    {
+        //campo y propiedad nombre
+        private string name = "";
+        public string Name { get => name; set => name = value; }
+
+        //campo y propiedad que almancena intentos
+        private List<int> listGuess;
+        public List<int> ListGuess { get => listGuess; set => listGuess = value; }
+
+        //constructor
+        protected Player(string name)
+        {
+            Name = name;
+            listGuess = new List<int>();
+        }
+
+        //metodo abstracto
+        public abstract int MakeGuess();
+    }
 }
 
